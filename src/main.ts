6a3a9e6602b64c173533c7be36606bc80acdb820
Sam10F <samueldf7994@gmail.com>
2024-01-02 22:42:22 +0100

    feat: :sparkles: vue add vuestic-ui

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createVuesticEssential, VaButton } from 'vuestic-ui'
+import 'vuestic-ui/styles/essential.css'
+import 'vuestic-ui/styles/grid.css'
+import 'vuestic-ui/styles/reset.css'
+import 'vuestic-ui/styles/typography.css'
 
-createApp(App).mount('#app')
+createApp(App).use(createVuesticEssential({ components: { VaButton } })).mount('#app')
