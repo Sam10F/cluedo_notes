12b1fb0ebade7fb4ee09b1dfbf57a64c77303316
Sam10F <samueldf7994@gmail.com>
2024-01-06 14:44:04 +0100

    feat: :sparkles: Add necessary icons to enable PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,40 @@
 import { defineConfig } from 'vite';
-import { VitePWA } from 'vite-plugin-pwa';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+        type: 'module',
+      },
+      manifest: {
+        name: 'Cluedo Notes',
+        start_url: '/',
+        short_name: 'CluedoNotes',
+        description: 'Add your cluedo notes here and win the game!',
+        theme_color: '#031c36',
+        icons: [
+          {
+            src: '/cluedo_icon-180x180.png',
+            sizes: '180x180',
+            type: 'image/png',
+          },
+          {
+            src: '/cluedo_icon-120x120.png',
+            sizes: '120x120',
+            type: 'image/png',
+          },
+          {
+            src: '/cluedo_icon-87x87.png',
+            sizes: '87x87',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 });
