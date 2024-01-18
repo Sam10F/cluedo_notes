8ca2c6a332fedc7a74427d9e2acf154913deb3df
Sam10F <samueldf7994@gmail.com>
2024-01-18 15:16:16 +0100

    build: :wrench: Add custom import paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 import { VitePWA } from 'vite-plugin-pwa';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
@@ -37,4 +38,7 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: [{ find: '∼', replacement: path.resolve(__dirname, 'src') }],
+  },
 });
