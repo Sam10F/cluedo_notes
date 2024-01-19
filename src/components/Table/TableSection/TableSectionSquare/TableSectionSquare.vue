8c0e014fe574592a5afccde950caf87bec2c1075
Sam10F <samueldf7994@gmail.com>
2024-01-19 12:15:44 +0100

    Add notes to table

--- src/components/Table/TableSection/TableSectionSquare/TableSectionSquare.vue
+++ src/components/Table/TableSection/TableSectionSquare/TableSectionSquare.vue
@@ -1,29 +1,93 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { VaPopover } from 'vuestic-ui';
+
+const activeIcon = ref<string>('');
+
+const setActiveIcon = (event: Event) => {
+  activeIcon.value = (event.target as HTMLElement).innerText;
+};
 </script>
 <template>
   <td class="TableSectionSquare">
     <VaPopover trigger="click" placement="bottom" :stick-to-edges="true">
-      <VaButton color="#00000000"></VaButton>
+      <VaButton color="#00000000" :icon="activeIcon"></VaButton>
 
       <template #title>
-        <VaButton color="danger" icon="close"></VaButton>
+        <VaButton
+          color="#FFFFFF"
+          icon="restart_alt"
+          :background-opacity="0"
+          size="large"
+          @click="activeIcon = ''"
+        ></VaButton>
+        <VaButton
+          color="#FFFFFF"
+          icon="close"
+          :background-opacity="0"
+          size="large"
+        ></VaButton>
       </template>
 
       <template #body>
-        <p>Suspendisse pharetra risus magna, in luctus turpis mollis non.</p>
-        <p>Donec est nulla, imperdiet id dapibus sed, consequat eget odio.</p>
+        <VaButton
+          color="secondary"
+          icon="check"
+          @click="e => setActiveIcon(e)"
+        ></VaButton>
+        <VaButton
+          color="secondary"
+          icon="done_all"
+          @click="e => setActiveIcon(e)"
+        ></VaButton>
+        <VaButton
+          color="secondary"
+          icon="question_mark"
+          @click="e => setActiveIcon(e)"
+        ></VaButton>
+        <VaButton
+          color="secondary"
+          icon="star"
+          @click="e => setActiveIcon(e)"
+        ></VaButton>
       </template>
     </VaPopover>
   </td>
 </template>
-<style lang="scss" scoped>
+<style lang="scss">
 .TableSectionSquare {
   text-align: center;
 
   .va-button {
+    display: flex;
     width: 100%;
     height: 100%;
+  }
+}
+
+.va-popover {
+  &__body {
+    display: flex;
+    gap: 8px;
+  }
+
+  &__title {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 12px;
+
+    button {
+      display: flex;
+      align-items: flex-start;
+      justify-content: center;
+      min-width: 20px;
+      min-height: 30px;
+      color: white;
+
+      i {
+        color: white;
+      }
+    }
   }
 }
 </style>
